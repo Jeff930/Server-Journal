7170ec054e1c343c0c8be8307c9635af9e7803dd
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-22 18:06:28 +0800

    Minor text changes

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const mysql = require('mysql');
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('APP SUCCESSFULLY HOSTED!'))
 
 app.listen(port, () => console.log(`Aapp listening at http://localhost:${port}`))
