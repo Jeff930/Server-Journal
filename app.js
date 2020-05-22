e46811aa260c7ddaa7b252f4ba170ec9e47c7e32
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-22 17:20:00 +0800

    Added mysql to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mysql = require('mysql');
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Aapp listening at http://localhost:${port}`))
