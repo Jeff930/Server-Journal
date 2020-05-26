b6d39f346075e45098f2454c94717b1179fee1be
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-26 16:16:07 +0800

    Added Body-parser dependency

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const connection = mysql.createPool({
     database: 'chandrajournal'
 });
 
-app.get('/', (req, res) => {
+app.get('/sample', (req, res) => {
     connection.query("SELECT * FROM `users`", (err, result) => {
         if (err) {
             console.log(err);
@@ -22,5 +22,44 @@ app.get('/', (req, res) => {
         }
     });
 });
+
+app.get('/user-login/:form', (req, res) => {
+    var domain = req.params.domain;
+    const form = JSON.parse(req.params.form);
+    var sql = "SELECT `UserId`,"+
+                    " `UserName`," +
+                    " `FirstName`," +
+                    " `LastName`,"+
+                    " `EmailAddress`"+
+                    " FROM `users` WHERE `EmailAddress` = '"+form.email+"'"+
+                    " AND `Password` = '"+md5(form.password)+"'";
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({ "error": err });
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+});
+
+app.get('/', (req, res) => {
+    var domain = req.params.domain;
+    const form = JSON.parse(req.params.form);
+    var sql = "SELECT * FROM `entries`";
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({ "error": err });
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+});
+
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
