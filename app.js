ec2be9f27e44dc86057de40bd4169a7455d63354
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-26 15:48:15 +0800

    Added MySQL dependency

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql')
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('APP SUCCESSFULLY HOSTED!'))
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'chandrajournal'
+});
+
+app.get('/', (req, res) => {
+    connection.query("SELECT * FROM `users`", (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({ "error": err });
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+});
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
