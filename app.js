ccfb1dbdb19fbc2f29189b362f2856891adf043a
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-26 17:10:00 +0800

    Created Login API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 var mysql = require('mysql')
+
 const port = process.env.PORT || 5000;
 
 const connection = mysql.createPool({
@@ -10,21 +11,8 @@ const connection = mysql.createPool({
     database: 'chandrajournal'
 });
 
-app.get('/sample', (req, res) => {
-    connection.query("SELECT * FROM `users`", (err, result) => {
-        if (err) {
-            console.log(err);
-            res.json({ "error": err });
-        }
-        else {
-            console.log(result);
-            res.send(result);
-        }
-    });
-});
-
+//USER APIs
 app.get('/user-login/:form', (req, res) => {
-    var domain = req.params.domain;
     const form = JSON.parse(req.params.form);
     var sql = "SELECT `UserId`,"+
                     " `UserName`," +
@@ -32,7 +20,7 @@ app.get('/user-login/:form', (req, res) => {
                     " `LastName`,"+
                     " `EmailAddress`"+
                     " FROM `users` WHERE `EmailAddress` = '"+form.email+"'"+
-                    " AND `Password` = '"+md5(form.password)+"'";
+                    " AND `Password` = '"+form.password+"'";
     connection.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -45,21 +33,6 @@ app.get('/user-login/:form', (req, res) => {
     });
 });
 
-app.get('/', (req, res) => {
-    var domain = req.params.domain;
-    const form = JSON.parse(req.params.form);
-    var sql = "SELECT * FROM `entries`";
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log(err);
-            res.json({ "error": err });
-        }
-        else {
-            console.log(result);
-            res.send(result);
-        }
-    });
-});
 
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
