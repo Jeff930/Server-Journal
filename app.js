24ac49bf8259b8721ddafe589aaf0182b4f1f58b
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-27 15:57:48 +0800

    Created Login API

--- app.js
+++ app.js
@@ -11,6 +11,31 @@ const connection = mysql.createPool({
     database: 'chandrajournal'
 });
 
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Check if app is served correctly
+app.get('/', (req, res) => 
+    res.send('App served successfully!'));
+
+app.get('/test', (req, res) => {
+    var sql = "Select * from `entries`";
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({ "error": err });
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+});
+
 //USER APIs
 app.get('/user-login/:form', (req, res) => {
     const form = JSON.parse(req.params.form);
